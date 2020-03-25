a7e1955f0f98bf0f5767384bff88432aa6a7d97e
suiXido <sprabin2@gmail.com>
2020-03-25 16:39:04 +0530

    Add files via upload

---
+++ appleblocking.py
@@ -0,0 +1,15 @@
+from subprocess import *
+
+#***********Pravin Kumar Singh(suixido)*************
+
+var=input("Enter Domain Name:")
+
+
+#In domain name field we should start by typing: domain="domain name"
+#Example: domain=pasteyourdomainname
+#and press enter
+
+call(["http", "--form", "POST", "http://128.199.154.155/api/setting/get_setting.php",var] )
+
+
+#***********16shop blocking script latest 2020*************
